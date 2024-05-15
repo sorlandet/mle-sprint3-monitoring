ff32acd1fb428c5cce63d169ae347da0efb79375
mle <mle>
2024-05-15 09:14:07 +0000

    t4l3

---
+++ fastapi_monitoring/main_app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from prometheus_fastapi_instrumentator import Instrumentator
+import numpy as np
+
+# создание экземпляра FastAPI приложения
+app = FastAPI()
+
+# инициализируем и запускаем экпортёр метрик
+instrumentator = Instrumentator()
+instrumentator.instrument(app).expose(app)
+
+# предсказания
+@app.get("/predict")
+def predict(x: int, y: int):
+    np.random.seed(x)
+    prediction = x+y + np.random.normal(0,1)
+    main_app_predictions.observe(prediction)
+    return {'prediction': prediction}
